6fca2cb0ad31ce2a3718131667f7f8f5e4412637
Tom Dhinsa <mrdhinsa05@gmail.com>
2022-03-14 11:31:19 -0400

    adds proper associations for user and reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
+
+      
   belongs_to :user                  
-  belongs_to :teacher, class_name:"User", foreign_key:"teacher_id"
+  belongs_to :teacher, class_name:"User",foreign_key:"teacher_id"
+  
+  
 end
