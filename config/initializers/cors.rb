34c7e90e1631ec6e1e518c30c2f9d6ed383d85e7
Tom Dhinsa <mrdhinsa05@gmail.com>
2022-03-17 15:05:33 -0400

    adds user show signup and index methods/pages

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,9 @@
-# config/initializers/cors.rb
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    origins "http://localhost:8080"
+
+    resource "*", 
+    headers: :any, 
+    methods: [:get, :post, :patch, :put, :delete, :options, :head]
   end
 end
