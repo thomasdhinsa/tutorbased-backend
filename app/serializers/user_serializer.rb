17cc378170e0bed30f519955a72a23f39b4bf4e0
Tom Dhinsa <mrdhinsa05@gmail.com>
2022-03-22 17:00:47 -0400

    adds is_teacher to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :education, :bio, :subjects, :zipcode, :preferred_contact, :image_url, :email
+  attributes :id, :name, :education, :bio, :subjects, :zipcode, :preferred_contact, :image_url, :email, :is_teacher
 
   has_many :reviews  
   has_many :earned_reviews 
